282a3c1db46cfe66ea885397d4676f9b843830de
Matthew Sallin <msallin@gmail.com>
2020-09-20 01:30:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,5 +5,5 @@ def meal_choice(veg1, veg2, protein = "tofu")
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("carrots","salad","chicken")
+#meal_choice("carrots","salad","chicken")
 meal_choice("carrots","salad")
